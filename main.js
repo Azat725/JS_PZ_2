731e7658de9ee36f370a8456cc15567492cb6678
Azat725 <152897735+Azat725@users.noreply.github.com>
2024-01-31 20:40:01 +0300

    сделаноооооо)))))))))))))

---
+++ main.js
@@ -0,0 +1,56 @@
+// let dayOfWeek = "Monday"
+
+// switch(dayOfWeek) {
+//     case "Sunday":
+//         console.log("Today Sunday")
+//         break;
+//     case "Monday":
+//         console.log("Today Monday")
+//         break;
+// }
+
+// for (let i = 0; i < 5; i ++) {
+//     console.log(i)
+// }
+
+// console.log("")
+
+// const array = [1, 3, 4, 5, 5, 6, 7, 78]
+
+// // for(i = 0; i < array.length; i++){
+// //     console.log(array[i])
+// // }
+
+// for (i of array){
+//     console.log(i)
+// }
+
+// console.log('')
+
+// for (i in array){
+//     console.log(i)
+// }
+
+
+// let y = 0
+
+// do{
+//     console.log(y)
+//     y++
+// }while(y < 4)
+
+let y = 5
+
+do{
+    console.log(y)
+    y++
+}while(y < 5)
+
+console.log("")
+
+let x = 0
+
+while(x < 5){
+    console.log(x)
+    x++
+}
